d241344c29a187b93e5be3a7fa80d45cc66ed1bd
markusrx <markus.marques@awscode.com.br>
2019-12-10 19:02:44 -0300

    pre pwa

--- time_bank_frontend/src/services/api.js
+++ time_bank_frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:1337',
+  baseURL: 'http://192.168.43.115:1337',
 })
 
 export const apiDATE = axios.create({
